374b8dab75ef1c5a4393874d0cfab7b8507182ff
taoguangc <taoguangc@gmail.com>
2025-04-15 13:56:24 +0800

    移除对 astro-compress 的依赖和相关配置

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
-import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,18 +12,6 @@ export default defineConfig({
     sitemap(),
     icon({
       iconDir: 'src/assets/icons'
-    }),
-    compress({
-      CSS: true,
-      HTML: {
-        'html-minifier-terser': {
-          removeAttributeQuotes: false
-        }
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: false,
-      Logger: 1
     })
   ],
   image: {
